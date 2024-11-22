357a5982551beafc8dbbfc2878fe3a554d20a192
Cecil Foubert <Cfoubert0782@conestogac.on.ca>
2024-11-22 13:57:15 -0500

    More Facility Work
    Will add map later, starting with arbitrary values for amount of sensors and points of entry

--- build/build_files/Facility-Handler.h
+++ build/build_files/Facility-Handler.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Facility-POE.h"
+#include "Facility-Sensor.h"
+
+#define POE_COUNT 5
+#define FACILITY_SENSOR_COUNT 7
+
+class Facility {
+	FacilitySensor sensors[FACILITY_SENSOR_COUNT];
+	POE pointsOfEntry[POE_COUNT];
+	bool isLockdown;
+
+public:
+	int engageLockdown();
+
+	void checkSensors();
+
+	void checkPOEs();
+
+};
--- build/build_files/Facility-POE.h
+++ build/build_files/Facility-POE.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <stdio.h>
+//POE short for point of entry
+class POE {
+	bool isBreached, isDamaged, isOpen;
+	int damageLvl, POEID, POEX1, POEX2, POEY1, POEY2;
+
+public:
+	POE();
+	void setPosition(int X1, int Y1, int X2, int Y2);
+	int getX1();
+	int getX2();
+	int getY1();
+	int getY2();
+
+	int getDamageLevel();
+	void setDamageLevel(int damageLvl);
+
+	bool getIsBreached();
+	void setIsBreached(bool isBreached);
+
+	bool getIsDamaged();
+	void setIsDamaged(bool isDamaged);
+
+	bool getIsOpen();
+	void setIsOpen(bool isOpen);
+
+
+};
