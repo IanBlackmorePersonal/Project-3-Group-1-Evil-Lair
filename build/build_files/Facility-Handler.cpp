bf7aaa26f4dd7e2edbfb4a7caa19d5c3e9f1acfa
Cecil Foubert <Cfoubert0782@conestogac.on.ca>
2024-11-29 00:07:58 -0500

    Basic basic facility funcitonality
    aaaaaaaaaaaa

--- build/build_files/Facility-Handler.cpp
+++ build/build_files/Facility-Handler.cpp
@@ -9,4 +9,37 @@ Facility::Facility()
 	for (int j = 0; j < POE_COUNT; j++) {
 		pointsOfEntry[j].makePOE(100, 100, 100, 100, j);
 	}
+	isLockdown = false;
 }
+
+int Facility::engageLockdown()
+{
+	for (int i = 0; i < FACILITY_SENSOR_COUNT; i++) {
+		sensors[i].setPowerState(true);
+	}
+	for (int j = 0; j < POE_COUNT; j++) {
+		if (pointsOfEntry[j].getIsBreached() == false)
+			pointsOfEntry[j].setIsOpen(false);
+	}
+	return 0;
+}
+
+void Facility::checkSensors()
+{
+	for (int i = 0; i < FACILITY_SENSOR_COUNT; i++) {
+		if (sensors[i].getTripped() == true) {
+			//something something update icon
+		}
+	}
+}
+
+void Facility::checkPOEs()
+{
+	for (int i = 0; i < FACILITY_SENSOR_COUNT; i++) {
+		if (pointsOfEntry[i].getIsBreached() == true) {
+			//something something update icon
+		}
+	}
+}
+
+
