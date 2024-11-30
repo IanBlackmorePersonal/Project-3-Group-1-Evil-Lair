1e30e3bdd3cb4fb86fb9ba13e5519bc84e94aad4
Dawson Fallows <Dfallows0672@conestogac.on.ca>
2024-11-30 14:33:13 -0500

    updated Anger Level to rise faster

--- build/build_files/MonsterContainmentUnit.cpp
+++ build/build_files/MonsterContainmentUnit.cpp
@@ -35,7 +35,7 @@ void MonsterContainmentUnit::releaseMonster() {
 
 void MonsterContainmentUnit::checkHungerLevel() {
    if (hungerLevel <= 25) {
-        angerLevel = max(0.0f, min(angerLevel + 0.0010f, 100.0f));
+        angerLevel = max(0.0f, min(angerLevel + 0.008f, 100.0f));
    }
 
 }
@@ -55,7 +55,7 @@ void MonsterContainmentUnit::updateHungerOverTime() {
 }
 
 void MonsterContainmentUnit::updateAngerOverTime() {
-    angerLevel = max(0.0f, min(angerLevel + (0.0005f), 100.0f));
+    angerLevel = max(0.0f, min(angerLevel + (0.003f), 100.0f));
     checkAngerLevel();
     
 }
